ddb83647a21f8d75f14036c7b895d964c723e9f5
Eugen Wiens <wiens.eugens@gmail.com>
2020-03-11 22:49:54 +0100

    Added first version of NeoPixelHandler
    This class wraps all stuff to comunicate with the NeoPixelLED an th Lib

---
+++ BLE GATT Server/include/NeoPixelHandler.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ColorHanlderInterface.hpp"
+#include "RgbColor.hpp"
+
+#include <Arduino.h>
+
+
+class NeoPixelHandler : public ColorHandlerInterface
+{
+public:
+    NeoPixelHandler();
+
+    void writeRgbColor(const RgbColor& rgbColor) const;
+
+    virtual void setColor(const RgbColor& rgbColor, uint32_t brightness) override;
+};
---
+++ BLE GATT Server/src/NeoPixelHandler.cpp
@@ -0,0 +1,16 @@
+#include "NeoPixelHandler.hpp"
+
+
+NeoPixelHandler::NeoPixelHandler()
+{
+}
+
+void NeoPixelHandler::writeRgbColor(const RgbColor& rgbColor) const
+{
+
+}
+
+void NeoPixelHandler::setColor(const RgbColor& rgbColor, uint32_t brightness)
+{
+
+}

ea733038394a2baf71bfb5281fdb69db6922a809
Eugen Wiens <wiens.eugens@gmail.com>
2020-05-11 22:50:22 +0200

    added logger and argparser (#19)
    * added logger and argparser
    * Update logger to select level via command line option (#22)
    
    
    Co-authored-by: Hermann Leinweber <hermann.leinweber@leinies.de>
    Co-authored-by: Eugen Wiens <wiens.eugens@gmail.com>

--- BLE-GATT-Client/src/jenkins_head_controller.py
+++ BLE-GATT-Client/src/jenkins_head_controller.py
@@ -1,10 +1,46 @@
+#!/usr/bin/env python3
 '''
-This module is used as entry point for the jenkens-head-controller
+This module is used as entry point for the jenkins-head-controller
 '''
+import argparse
+import logging
+
+versionNumber = '0.0.1'
+
+formatMap = {
+    'DEBUG': '%(threadName)-12s: %(levelname)-8s %(asctime)s - %(message)s',
+    'INFO': '%(asctime)s - %(message)s',
+    'WARNING': '%(asctime)s - %(message)s',
+    'ERROR': '%(asctime)s - %(message)s'
+}
+
+
+def initArgumentParser():
+    global versionNumber
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version', version='%(prog)s ' + versionNumber)
+    parser.add_argument(
+        '--log',
+        dest='logLevel',
+        type=str,
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'],
+        help='set the level of the logging, default=\'INFO\'',
+        default='INFO')
+    return parser.parse_args()
+
+
+def initLogger(logLevel):
+    '''
+    set up the logging framework regarding the logging level
+    '''
+    logFormatString = formatMap[logLevel]
+    logging.basicConfig(level=logLevel, format=logFormatString)
 
 
 def main():
-    print('Hello World from ' + __file__)
+    arguments = initArgumentParser()
+    initLogger(arguments.logLevel)
+    logging.info("starting...")
 
 
 if __name__ == "__main__":

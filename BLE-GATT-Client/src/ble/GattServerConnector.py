e5e8eec6b6e962e94cc36216658cee2da02348c1
Eugen Wiens <wiens.eugens@gmail.com>
2020-09-19 23:00:19 +0200

    added first test for the GattServerConnector

--- BLE-GATT-Client/src/ble/GattServerConnector.py
+++ BLE-GATT-Client/src/ble/GattServerConnector.py
@@ -1,7 +1,25 @@
 class GattServerConnector(object):
 
-    def __init__(self, bleAddress):
+    __rgbMap = {
+        'SUCCESS': {
+            'red': 0,
+            'green': 255,
+            'blue': 0
+        },
+        'FAILURE': {
+            'red': 255,
+            'green': 0,
+            'blue': 0
+        },
+    }
+
+    def __init__(self, bleAddress: str):
         self.__bleAddress = bleAddress
 
     def sendStatus(self, status: str):
+        rgbDict = self.__rgbMap[status]
+        if rgbDict:
+            self.__setRgbValues(rgbDict['red'], rgbDict['green'], rgbDict['blue'])
+
+    def __setRgbValues(self, red: int, green: int, blue: int):
         pass
---
+++ BLE-GATT-Client/tests/ble/GattServerConnector/test_GattServerConnector.py
@@ -0,0 +1,31 @@
+
+import pytest
+import mock
+import unittest
+
+from ble.GattServerConnector import GattServerConnector
+
+# this 'other' include form is needed, so that it is possible to mock this two modules
+# import jenkins_jobmanager.JenkinsJobManager as jobmanager
+
+
+@pytest.mark.unit_test
+class TestHeaderStatusDecisionManager(object):
+
+    @pytest.mark.parametrize('status, redValue, greenValue, blueValue', [
+        ('SUCCESS', 0, 255, 0),
+        ('FAILURE', 255, 0, 0),
+        ('FAILURE_WRONG', None, None, None),
+        ('SUCCESS_WRONG', None, None, None)
+    ])
+    @mock.patch.object(GattServerConnector, '_GattServerConnector__setRgbValues')
+    def test_SendStatus(self, mock_setRgbValues, status, redValue, greenValue, blueValue):
+        gattServerConnector = GattServerConnector('00:00:00:00:00:00')
+
+        if (redValue and greenValue and blueValue) is None:
+            with pytest.raises(KeyError):
+                gattServerConnector.sendStatus(status)
+                assert mock_setRgbValues.called == 0
+        else:
+            gattServerConnector.sendStatus(status)
+            mock_setRgbValues.mock_calls == mock.call(redValue, greenValue, blueValue)
